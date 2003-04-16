fd6b38dad6320bceac294635a39df42b4ccc0c04
Benjamin Smith <bsmith@cpan.org>
2003-04-16 14:21:21 +0000

    Import of BSMITH/Chatbot-Integral-0.0.01 from CPAN.
    gitpan-cpan-distribution: Chatbot-Integral
    gitpan-cpan-version:      0.0.01
    gitpan-cpan-path:         BSMITH/Chatbot-Integral-0.0.01.tar.gz
    gitpan-cpan-author:       BSMITH
    gitpan-cpan-maturity:     released

---
+++ Integral.pm
@@ -0,0 +1,64 @@
+package Chatbot::Integral;
+
+use 5.006001; # just for fun!
+use strict;
+use warnings;
+
+our $VERSION = '0.0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Chatbot::Integral - Chat like integral
+
+=head1 SYNOPSIS
+
+  use Chatbot::Integral;
+  blah blah blah
+
+=head1 ABSTRACT
+
+  This should be the abstract for Chatbot::Integral.
+  The abstract is used when making PPD (Perl Package Description) files.
+  If you don't want an ABSTRACT you should also edit Makefile.PL to
+  remove the ABSTRACT_FROM option.
+
+=head1 DESCRIPTION
+
+Stub documentation for Chatbot::Integral, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Development, E<lt>develop@internal.cyberhqz.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2003 by Development
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
